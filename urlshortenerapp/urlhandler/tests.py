479fb4648b58f81962ae307d4222ade1e2c0a779
Arielv1 <arielv1994@gmail.com>
2021-11-15 22:15:52 +0200

    postman + test cases

--- urlshortenerapp/urlhandler/tests.py
+++ urlshortenerapp/urlhandler/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from .models import ShortLongUrl
 
-# Create your tests here.
+
+class ShortLongUrlTestCases(TestCase):
+    def test_different_results_for_same_url(self):
+        num_to_test = 10
+        for i in range(0, num_to_test):
+            self.client.post(
+                "/create/", {'url': 'https://ravkavonline.co.il'})
+
+        all_urls = ShortLongUrl.objects.all()
+        count = 0
+        for url in all_urls:
+            count += len(ShortLongUrl.objects.filter(short=url.short))
+        self.assertEqual(count, len(all_urls))
+
+    def test_non_existing_short_url(self):
+        num_to_fill = 10
+        for i in range(0, num_to_fill):
+            self.client.post(
+                "/create/", {'url': 'https://ravkavonline.co.il'})
+
+        short_to_test = "uftyfa"
+        check = ShortLongUrl.objects.filter(short=short_to_test).exists()
+        self.assertEqual(check, False)
+
+    def test_incremented_times_used(self):
+        org_url = 'https://ravkavonline.co.il'
+        self.client.post(
+            "/create/", {'url': 'https://ravkavonline.co.il'})
+        short_url = ShortLongUrl.objects.all()[0]
+        self.assertEqual(short_url.long, org_url)
--- urlshortenerapp/urlhandler/utils.py
+++ urlshortenerapp/urlhandler/utils.py
@@ -3,16 +3,17 @@ from random import choice
 from string import ascii_letters, digits
 import hashlib
 
-SIZE = getattr(settings, "MAXIMUM_URL_CHARS", 6)
-salt = 0
+SHORT_URL_SIZE = getattr(settings, "MAXIMUM_URL_CHARS", 6)
 POSSIBLE_CHARS = ascii_letters + digits
+
 
 def compress_url(model, string):
     # Create salt to add volatility
     salt = choice(POSSIBLE_CHARS).encode()
 
     # Generate short url cropped by SIZE value
-    short_url = hashlib.sha256(string.encode() + salt).hexdigest()[:SIZE]
+    short_url = hashlib.sha256(
+        string.encode() + salt).hexdigest()[:SHORT_URL_SIZE]
 
     # Small chance that the random short url has already been generated -> generate again
     model_class = model.__class__
